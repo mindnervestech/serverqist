9f76d97a394a5efa5e0307c92d7286c64034ac53
mindnervestech <mindnervestech@gmail.com>
2015-05-19 12:17:30 +0530

    login bug

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -9,7 +9,12 @@ import java.util.HashMap;
 import java.util.Properties;
 import java.util.UUID;
 
-import javax.mail.*;
+import javax.mail.BodyPart;
+import javax.mail.Message;
+import javax.mail.Multipart;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
@@ -17,10 +22,6 @@ import javax.mail.internet.MimeMultipart;
 
 import models.Customer;
 import models.Product;
-
-import org.codehaus.jackson.JsonNode;
-import org.codehaus.jackson.map.ObjectMapper;
-
 import play.data.DynamicForm;
 import play.data.Form;
 import play.libs.Json;
@@ -29,7 +30,6 @@ import play.mvc.Http.MultipartFormData;
 import play.mvc.Http.MultipartFormData.FilePart;
 import play.mvc.Result;
 import sun.misc.BASE64Encoder;
-import viewmodels.CustomerVM;
 import viewmodels.ProductVM;
 import views.html.index;
 
@@ -77,33 +77,32 @@ public class Application extends Controller {
     	 String FacebookId = users.get("FacebookId");
     	 String GooglePlusId = users.get("GooglePlusId");
     	 String TwitterId = users.get("TwitterId");
-    	 String img = users.get("image");
     	 String imageDataString = null ;
     	 Customer c = new Customer();
 		MultipartFormData body = request().body().asMultipartFormData();
 		if (body != null) {
 		FilePart filePart = body.getFile("image");
 		if (filePart != null) {
-		File f = filePart.getFile();
-		System.out.println("imageee file===="+f);
+			File f = filePart.getFile();
+			System.out.println("imageee file===="+f);
+			 
+			 try {            
+		            FileInputStream imageInFile = new FileInputStream(f);
+		            byte imageData[] = new byte[(int) f.length()];
+		            imageInFile.read(imageData);
+		            BASE64Encoder encoder = new BASE64Encoder();
+		            imageDataString = encoder.encode(imageData);
+		            System.out.println("imageee===="+imageDataString);
+		            imageInFile.close();
 		 
-		 try {            
-	            FileInputStream imageInFile = new FileInputStream(f);
-	            byte imageData[] = new byte[(int) f.length()];
-	            imageInFile.read(imageData);
-	            BASE64Encoder encoder = new BASE64Encoder();
-	            imageDataString = encoder.encode(imageData);
-	            System.out.println("imageee===="+imageDataString);
-	            imageInFile.close();
-	 
-	        } catch (FileNotFoundException e) {
-	            System.out.println("Image not found" + e);
-	        } catch (IOException ioe) {
-	            System.out.println("Exception while reading the Image " + ioe);
-	        }
-		
+		        } catch (FileNotFoundException e) {
+		            System.out.println("Image not found" + e);
+		        } catch (IOException ioe) {
+		            System.out.println("Exception while reading the Image " + ioe);
+		        }
+			
+			}
 		}
-	}
 		if(fname.isEmpty() || fname == null ||
 				lname.isEmpty() || lname == null){
 			return ok(Json.toJson(new ErrorResponse(Error.E500.getCode(), Error.E500.getMessage())));
@@ -116,7 +115,7 @@ public class Application extends Controller {
 		c.lastName = lname;
 		c.email = email;
 		c.password = pass;
-		c.address = repass;
+		c.address = address;
 		c.image = imageDataString;
 		c.qCartMailingList = qcart;
 		c.type = "M";
@@ -166,19 +165,12 @@ public class Application extends Controller {
     }
     
     public static Result login(){
-    	JsonNode json = request().body().asJson();
-		ObjectMapper mapper = new ObjectMapper();
-		CustomerVM customer = null;
-		try {
-			customer = mapper.readValue(json.traverse(),CustomerVM.class);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		if(customer.email.isEmpty() || customer.email == null ||
-				customer.password.isEmpty() || customer.password == null){
+    	DynamicForm users = Form.form().bindFromRequest();
+		if(users.get("email").isEmpty() || users.get("email") == null ||
+				users.get("password").isEmpty() || users.get("password") == null){
 			return ok(Json.toJson(new ErrorResponse(Error.E500.getCode(), Error.E500.getMessage())));
 		}
-		Customer c = Customer.findByEmailAndPassword(customer.email, customer.password);
+		Customer c = Customer.findByEmailAndPassword(users.get("email"), users.get("password"));
 		if(c == null){
 			return ok(Json.toJson(new ErrorResponse(Error.E500.getCode(), Error.E500.getMessage())));
 		}
